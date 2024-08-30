ca75cd66a63536d4b99cac3edf457bffe84f89ea
jondsonjony <jonson.jony27@gmail.com>
2024-08-31 00:05:02 +0900

    Initial Commit

---
+++ Club37/Club37App.swift
@@ -0,0 +1,17 @@
+//
+//  Club37App.swift
+//  Club37
+//
+//  Created by 庄司有希 on 2024/08/31.
+//
+
+import SwiftUI
+
+@main
+struct Club37App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
