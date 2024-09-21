0aa0cd2cc209942e3082d27fc7d5f0ee343eea75
yukishoji <jonson.jony27@gmail.com>
2024-09-21 15:21:56 +0900

    simulatorでは写真撮影が出来ないので決まった画像を参照するように

--- Club37/ImagePickerView.swift
+++ Club37/ImagePickerView.swift
@@ -28,14 +28,32 @@ struct ImagePickerView: UIViewControllerRepresentable {
         return Coordinator(parent: self)
     }
     
-    func makeUIViewController(context: Context) -> UIImagePickerController {
+    func makeUIViewController(context: Context) -> UIViewController {
+        #if targetEnvironment(simulator)
+        // シミュレーターの場合はカスタムのViewControllerで固定画像を選択
+        let fixedImageViewController = UIViewController()
+        let imageView = UIImageView(image: UIImage(systemName: "photo")) // 固定の画像を設定
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = fixedImageViewController.view.frame
+        fixedImageViewController.view.addSubview(imageView)
+        
+        // シミュレーターの場合は直接画像を選択する動作にする
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.selectedImage = UIImage(systemName: "photo") // 固定の画像を設定
+            self.isPresented = false
+        }
+        return fixedImageViewController
+        
+        #else
+        // 実機ではカメラを使用
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
         picker.sourceType = .camera
         return picker
+        #endif
     }
     
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         // Do nothing
     }
 }
