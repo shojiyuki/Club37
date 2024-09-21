35e10d58b5b65ce7c577c4f4780f50e1f77aecef
yukishoji <jonson.jony27@gmail.com>
2024-09-21 19:21:54 +0900

    modal to navigation

--- Club37/ContentView.swift
+++ Club37/ContentView.swift
@@ -1,13 +1,15 @@
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
     let items: [ListItem] = sampleListItems
     
     @State private var currentIndex: Int = 0
     @State private var capturedImage: UIImage?
+    @State private var path = NavigationPath()
     
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $path) {
             VStack {
                 CountdownWrapperView(selectedItemTime: items[currentIndex].time) { isCheckInAllowed, timeRemaining in
                     VStack {
@@ -18,20 +20,28 @@ struct ContentView: View {
                         Spacer()
                         
                         if isCheckInAllowed {
-                            NavigationLink(destination: ImagePickerView(selectedImage: $capturedImage)) {
-                                CheckInButton {}
-                                    .padding(.bottom, 30)
+                            CheckInButton {
+                                path.append("ImagePicker")  // 識別子を追加して遷移を制御
                             }
+                            .padding(.bottom, 30)
                         }
                     }
                     .background(Color.black.edgesIgnoringSafeArea(.all))
                 }
-                .id(currentIndex)  // ここで CountdownWrapperView を currentIndex の変更に応じて再生成する
+                .id(currentIndex)  // currentIndex の変更に応じて再生成
+            }
+            .navigationDestination(for: String.self) { view in  // 識別子に基づいた遷移を定義
+                if view == "ImagePicker" {
+                    ImagePickerView(selectedImage: $capturedImage)
+                        .onChange(of: capturedImage) {
+                            if capturedImage != nil {
+                                path.append("ImagePreview")
+                            }
+                        }
+                } else if view == "ImagePreview" {
+                    ImagePreviewView(image: $capturedImage)
+                }
             }
         }
     }
-}
-
-#Preview {
-    ContentView()
 }
